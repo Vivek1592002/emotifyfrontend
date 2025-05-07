2889e047714d60f7e5724fb1e16b27e97cd66628
Vivek shekhawat <111676030+Vivek1592002@users.noreply.github.com>
2025-05-07 17:33:53 +0530

    Update About.js

--- src/About.js
+++ src/About.js
@@ -20,7 +20,7 @@ function About(){
             </nav>
         <div style={{color:"white", fontFamily:"cursive", fontSize:"30px", margin:"30px"}}>
            
-            <center><img src={eye} style={{height:"180px", width:"300px"}}></img><h1>ANKH</h1></center>
+            <center><img src={eye} style={{height:"180px", width:"300px"}}></img><h1>EMOTIFY</h1></center>
             <p>This Project focuses on music for all emotions , whether you are happy sad or suffering a heart break <br></br>
                 our app doesnt need to be told anything it reads you emotion like a friend and plays a song to sothen your heart.
             </p>
